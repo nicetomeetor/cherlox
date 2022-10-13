ff16d4cd3dc25ac3c0cd58215c5ce2bd11a3a0af
Michael Lutsiv <nicetomeetor@gmail.com>
2022-10-13 12:50:05 +0700

    Make constants for tokens

--- main.go
+++ main.go
@@ -3,6 +3,61 @@ package main
 import (
 	"fmt"
 	"os"
+	"bufio"
+	"log"
+)
+
+type Direction int
+
+const (
+    // Single-character tokens.
+  	LEFT_PAREN Direction  = iota 
+  	RIGHT_PAREN 
+	LEFT_BRACE
+	RIGHT_BRACE
+  	COMMA 
+	DOT 
+	MINUS 
+	PLUS 
+	SEMICOLON 
+	SLASH 
+	STAR
+
+  	// One or two character tokens.
+  	BANG 
+	BANG_EQUAL
+  	EQUAL 
+	EQUAL_EQUAL
+  	GREATER 
+	GREATER_EQUAL
+  	LESS 
+	LESS_EQUAL
+
+  	// Literals.
+  	IDENTIFIER 
+	STRING
+	NUMBER
+
+  	// Keywords.
+  	AND
+	CLASS 
+	ELSE 
+	FALSE 
+	FUN 
+	FOR 
+	IF
+	NIL 
+	OR
+  	PRINT 
+	RETURN 
+	SUPER 
+	THIS
+	TRUE 
+	VAR 
+	WHILE
+
+	// End of file.
+  	EOF
 )
 
 func main() {
