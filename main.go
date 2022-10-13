fad500e757ec7b1345accc0acbaf134f0434ee72
Michael Lutsiv <nicetomeetor@gmail.com>
2022-10-13 17:10:38 +0700

    Move token to package

--- main.go
+++ main.go
@@ -1,90 +1,168 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"bufio"
+	"fmt"
 	"log"
+	"os"
+	"token"
 )
 
-type TokenType int
-
-const (
-    // Single-character tokens.
-  	LEFT_PAREN TokenType  = iota 
-  	RIGHT_PAREN 
-	LEFT_BRACE
-	RIGHT_BRACE
-  	COMMA 
-	DOT 
-	MINUS 
-	PLUS 
-	SEMICOLON 
-	SLASH 
-	STAR
-
-  	// One or two character tokens.
-  	BANG 
-	BANG_EQUAL
-  	EQUAL 
-	EQUAL_EQUAL
-  	GREATER 
-	GREATER_EQUAL
-  	LESS 
-	LESS_EQUAL
-
-  	// Literals.
-  	IDENTIFIER 
-	STRING
-	NUMBER
-
-  	// Keywords.
-  	AND
-	CLASS 
-	ELSE 
-	FALSE 
-	FUN 
-	FOR 
-	IF
-	NIL 
-	OR
-  	PRINT 
-	RETURN 
-	SUPER 
-	THIS
-	TRUE 
-	VAR 
-	WHILE
-
-	// End of file.
-  	EOF
-)
-
-type Token struct {
-	tokenType TokenType
-	lexeme string
-	literal int
-	line int
+func error(line int, message string) {
+	report(line, "", message)
 }
 
-func (t *Token) toString() {
-	concatenated := fmt.Sprintf("%d %s %d", t.tokenType, t.lexeme, t.literal)
+func report(line int, where string, message string) {
+	concatenated := fmt.Sprintf("[line %d] Error %s: %s", line, where, message)
 
-    fmt.Println(concatenated)
+	fmt.Println(concatenated)
+}
+
+type Scanner struct {
+	source  string
+	tokens  []Token
+	start   int //0
+	current int // 0
+	line    int //= 1;
+}
+
+func (s *Scanner) isAtEnd() bool {
+	return s.current >= len(s.source)
+}
+
+func (s *Scanner) scanToken() {
+	c := s.advance()
+
+	switch c {
+	case "(":
+		s.addToken(LEFT_PAREN)
+	case ")":
+		s.addToken(RIGTH_PAREN)
+	case "}":
+		s.addToken(LEFT_BRACE)
+	case "{":
+		s.addToken(RIGHT_BRACE)
+	case ",":
+		s.addToken(COMMA)
+	case ".":
+		s.addToken(DOT)
+	case "-":
+		s.addToken(MINUS)
+	case "+":
+		s.addToken(PLUS)
+	case "*":
+		s.addToken(STAR)
+	case ";":
+		s.addToken(SEMICOLON)
+
+	case '!':
+		if s.match('=') {
+			s.addToken(BANG_EQUAL)
+		} else {
+			s.addToken(BANG)
+		}
+	case '=':
+		if s.match('=') {
+			s.addToken(EQUAL_EQUAL)
+		} else {
+			s.addToken(EQUAL)
+		}
+	case '<':
+		if s.match('=') {
+			s.addToken(LESS_EQUAL)
+		} else {
+			s.addToken(LESS)
+		}
+	case '>':
+		if s.match('=') {
+			s.addToken(GREATER_EQUAL)
+		} else {
+			s.addToken(GREATER)
+		}
+
+	case '/':
+		if match('/') {
+			// A comment goes until the end of the line.
+
+			for s.peek() != '\n' && !s.isAtEnd() {
+				s.advance()
+			}
+		} else {
+			s.addToken(SLASH)
+		}
+
+	case ' ':
+	case '\r':
+	case '\t':
+		// Ignore whitespace.
+
+	case '\n':
+		s.line++
+
+	default:
+		error(s.line, "Unexpected character.")
+	}
+}
+
+func (s *Scanner) peek() string {
+	if s.isAtEnd() {
+		return "\\0"
+	}
+
+	return s.source[s.current]
+}
+
+func (s *Scanner) match(expected string) bool {
+	if s.isAtEnd() {
+		return false
+	}
+
+	if s.source[s.current] != expected {
+		return false
+	}
+
+	s.current++
+	return true
+}
+
+func (s *Scanner) advance() string {
+	value := s.current
+	s.current = s.current + 1
+	return s.source[value]
+}
+
+func (s *Scanner) addToken(tokenType TokenType) {
+	//addToken(type, null);
+	text := s.source[s.start:s.current]
+	append(s.tokens, TokensToken{tokenType, text, s.literal, s.line})
+}
+
+func (s *Scanner) scanTokens() {
+	for !s.isAtEnd() {
+		s.start = s.current
+		scanToken()
+	}
+
+	token := Token{EOF, "", nil, line}
+
+	append(s.tokens, token)
+
+	return s.tokens
 }
 
 func main() {
-    args := os.Args[1:]
+	args := os.Args[1:]
 
-	if (len(args) > 1) {
+	if len(args) > 1 {
 		fmt.Println("Usage: cher [script]")
 		os.Exit(0)
- 	} else if (len(args) == 1) {
+	} else if len(args) == 1 {
 		fmt.Println("Run: cher [file]")
-  	} else {
+		runFile(args[0])
+	} else {
 		fmt.Println("Run: cher [prompt]")
 		runPrompt()
-  	}
+	}
 }
 
 func runPrompt() {
@@ -98,8 +176,12 @@ func runPrompt() {
 			log.Fatal(err)
 		}
 
-		if (line == "exit\n") {
+		if line == "exit\n" {
 			os.Exit(0)
 		}
 	}
+}
+
+func runFile(path string) {
+
 }
---
+++ token/token.go
@@ -0,0 +1,68 @@
+package token
+
+import "fmt"
+
+type TokenType string
+
+const (
+	// Single-character tokens.
+	LEFT_PAREN  TokenType = "("
+	RIGHT_PAREN           = ")"
+	LEFT_BRACE            = "{"
+	RIGHT_BRACE           = "}"
+	COMMA                 = ","
+	DOT                   = "."
+	MINUS                 = "-"
+	PLUS                  = "+"
+	SEMICOLON             = ";"
+	SLASH                 = "/"
+	STAR                  = "*"
+
+	// One or two character tokens.
+	BANG          = "!"
+	BANG_EQUAL    = "!="
+	EQUAL         = "="
+	EQUAL_EQUAL   = "=="
+	GREATER       = ">"
+	GREATER_EQUAL = ">="
+	LESS          = "<"
+	LESS_EQUAL    = "<="
+
+	// Literals.
+	IDENTIFIER = "identifier"
+	STRING     = "string"
+	NUMBER     = "number"
+
+	// Keywords.
+	AND    = "and"
+	CLASS  = "class"
+	ELSE   = "else"
+	FALSE  = "false"
+	FUN    = "fun"
+	FOR    = "for"
+	IF     = "if"
+	NIL    = "nil"
+	OR     = "or"
+	PRINT  = "print"
+	RETURN = "return"
+	SUPER  = "super"
+	THIS   = "this"
+	TRUE   = "true"
+	VAR    = "var"
+	WHILE  = "while"
+
+	// End of file.
+	EOF = "eof"
+)
+
+//Token contains the lexeme read by the scanner
+type Token struct {
+	Type    TokenType
+	Lexeme  string
+	Literal interface{}
+	Line    int
+}
+
+func (t *Token) String() string {
+	return fmt.Sprintf("%s %s %v", t.Type, t.Lexeme, t.Literal)
+}
